f26ca7c2c3eaccaf6f2e6ae3d7c5a7999a84d884
luchodeco <72599025+louisdeco@users.noreply.github.com>
2025-04-07 14:35:33 +0400

    fix: Update webpack output path to root-level

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,7 @@ export default {
   entry: './src/battleship.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     clean: true,
   },
   module: {
